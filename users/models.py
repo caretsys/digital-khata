5e5c4eaa83a3e03f1b3e4d4537a38d81c9525c72
Anil Poudyal <anilrunjakote@gmail.com>
2020-06-20 20:44:42 +0545

    User Models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customer(models.Model):
+    # user personal info
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=200, null=True)
+    gender = models.CharField(max_length=20)
+    store_name = models.CharField(max_length=20)
+
+    # address
+    country = models.CharField(max_length=20, default="Nepal")
+    city = models.CharField(max_length=20, default="Kathmandu")
+    street = models.CharField(
+        max_length=50, default="Please Update Your Sreet")
+    landmark = models.CharField(
+        max_length=50, default="Please Update Your Sreet")
+
+    def __str__(self):
+        return self.user.username
